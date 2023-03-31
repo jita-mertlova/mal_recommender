6cac07a7bfa72fc6e737e5ffc6be21526e76e1f1
Jitka Mertlova <jitka.mertlova@kuleuven.be>
2023-03-31 23:43:26 +0200

    bug somewhere

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,36 @@
 # here we will store standard routes for the website (e.g. where users can actually go to
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from . import db   ##means from __init__.py import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template("login.html", myVariable=False)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 def logout():
-    return "LogOUT"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -20,7 +39,11 @@ def sign_up():
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        if len(email) < 2:
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 2:
             flash('Email must be at least 2 characters.', category='error')
         elif len(first_name) < 2:
                 flash('Name must be at least 2 characters.', category='error')
@@ -28,4 +51,11 @@ def sign_up():
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 2:
             flash('Password must be at least 2 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
+            flash('Account created!', category=email)
+            return redirect(url_for('views.home'))    # i am finding what url maps to the home function in views.py
     return render_template("sign_up.html")
--- website/views.py
+++ website/views.py
@@ -1,9 +1,10 @@
 # here we will store standard routes for the website (e.g. where users can actuallz go to
 from flask import Blueprint, render_template
-
+from flask_login import login_required, current_user
 views = Blueprint('views', __name__)
 
 @views.route('/') # slash because main paige of the website
+@login_required
 def home():
     return render_template("home.html")
 
